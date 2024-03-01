932cb81ecd202f6d5685e05b79b3853187d9270f
Vex-7-Games <161663844+Vex-7-Games@users.noreply.github.com>
2024-03-01 07:04:23 -0600

    1

---
+++ Assets/scripts/Script.js
@@ -0,0 +1,13 @@
+"use strict";
+var is_fullscreen = false;
+function open_fullscreen() {
+			if (game.requestFullscreen) {
+				game.requestFullscreen();
+			} else if (game.mozRequestFullScreen) { /* Firefox */
+				game.mozRequestFullScreen();
+			} else if (game.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+				game.webkitRequestFullscreen();
+			} else if (game.msRequestFullscreen) { /* IE/Edge */
+				game.msRequestFullscreen();
+			}
+		}
